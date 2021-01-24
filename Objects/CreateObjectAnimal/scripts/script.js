ea0d824e9abd4d4993eae5b6f8e419b30304c941
Planke85 <coa85@outlook.com>
2021-01-24 09:13:50 +0100

    First object exercise

---
+++ Objects/CreateObjectAnimal/scripts/script.js
@@ -0,0 +1,39 @@
+let name = document.getElementById("name");
+let kind = document.getElementById("animKind");
+
+function Animal(name,kind,speak){
+    this.name = name;
+    this.kind = kind;
+    this.speak = function(){
+        return document.getElementById("text").innerHTML = `${this.kind} with name ${this.name} says: Hey Bro :) !!!!`;
+    }
+}
+
+function validate(name,kind){
+    if(name.trim() === ""){
+        document.getElementsByClassName("error")[0].innerHTML = `* Please enter name!`;
+    }
+    else if(!isNaN(parseInt(name))){
+        document.getElementsByClassName("error")[0].innerHTML = `* Name must be a string!`;
+    }
+    else if(kind.trim() === ""){
+        document.getElementsByClassName("error")[0].innerHTML = "";
+        document.getElementsByClassName("error")[1].innerHTML = `* Please enter kind!`;
+    }
+    else if(!isNaN(parseInt(kind))){
+        document.getElementsByClassName("error")[1].innerHTML = `* Kind must be a string!`;
+    }
+    else{
+        document.getElementsByClassName("error")[1].innerHTML = "";
+        createAnimal(name,kind);
+    }
+}
+
+function createAnimal(name,kind,speak){
+    let animal = new Animal(name, kind, speak);
+    animal.speak();
+}
+
+document.getElementById("btn").addEventListener("click", function(){
+    validate(name.value, kind.value);
+});
